122b1302e80e372eb21bed4421eb5f51866df087
grigi_ <grigi_@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-01-15 15:41:10 +0000

    Updated strlcpy and strlcat from OpenBSD sources, including license notice

--- src/strl.c
+++ src/strl.c
@@ -20,36 +20,91 @@
 // strlcpy and strlcpy implementation from OpenBSD 
 // To find out about strl(cat/cpy) read:
 // http://www.gratisoft.us/todd/papers/strlcpy.html
+/*
+ * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 #include <string.h>
 #define STRL_C
 #include "strl.h"
 
 #if !defined strlcpy
-STATIN unsigned int strlcpy (char *dest, char *src, unsigned int size)
+/*
+ * Copy src to string dst of size siz.  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+STATIN size_t strlcpy(char *dst, const char *src, size_t siz)
 {
-	register unsigned int i;
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
 
-	for (i=0; size > 0 && src[i] != '\0'; ++i, size--)
-		dest[i] = src[i];
+	/* Copy as many bytes as will fit */
+	if (n != 0) {
+		while (--n != 0) {
+			if ((*d++ = *s++) == '\0')
+				break;
+		}
+	}
 
-	dest[i] = '\0';
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0) {
+		if (siz != 0)
+			*d = '\0';		/* NUL-terminate dst */
+		while (*s++)
+			;
+	}
 
-	return i;
+	return(s - src - 1);	/* count does not include NUL */
 }
 #endif
 
 #if !defined strlcat
-STATIN unsigned int strlcat (char *dest, char *src, unsigned int size)
+/*
+ * Appends src to string dst of size siz (unlike strncat, siz is the
+ * full size of dst, not space left).  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
+ * Returns strlen(src) + MIN(siz, strlen(initial dst)).
+ * If retval >= siz, truncation occurred.
+ */
+STATIN size_t strlcat(char *dst, const char *src, size_t siz)
 {
-	register char *d = dest, *s = src;
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	size_t dlen;
 
-	for (; size > 0 && *d != '\0'; size--, d++);
-	for (; size > 0 && *s != '\0'; size--, d++, s++)
-		*d = *s;
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
 
+	if (n == 0)
+		return(dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
 	*d = '\0';
-	return (d - dest) + (s - src);
+
+	return(dlen + (s - src));	/* count does not include NUL */
 }
 #endif
 
