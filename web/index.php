103e91a95853377d06768414594b199a45068dbf
grigi_ <grigi_@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-01-25 12:55:37 +0000

    Shuffled the repos to a more sensible layout PLEASE BE CAREFUL WHEN UPDATING TO THIS REVISION, BACK UP YOUR OWN CODE

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<html>
+<head>
+<title>MiniWebsvr</title>
+</head>
+<body>
+<h1>MiniWebsvr</h1>
+<hr>
+This page is under construction, in the meantime go to the SourceFORGE.net project page:
+<a href="http://sourceforge.net/projects/miniwebsvr/">http://sourceforge.net/projects/miniwebsvr/</a>
+<hr
+<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=187000&amp;type=5" width="210" height="62" border="0" alt="SourceForge.net Logo" /></a>
+</body>
+</html>
