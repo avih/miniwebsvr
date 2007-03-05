f3ea3f19c3c5b032109e742758fce3bc4b97769a
grigi_ <grigi_@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-03-05 09:46:03 +0000

    - added google analytics urchintracker to pages :)

--- web/lib.php
+++ web/lib.php
@@ -74,7 +74,12 @@ a.sidebar:hover { color: yellow; }
 function endpage() { ?>
 </td>
 </table>
-
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1455258-1";
+urchinTracker();
+</script>
 </body>
 </html>
 <?}?>
