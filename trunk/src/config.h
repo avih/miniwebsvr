0ce70bd4a776038e5aebc3841a1a12ff89104179
grigi_ <grigi_@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-03-10 06:42:41 +0000

    Release current trunk as 0.0.7 It already has 3 security fixes, and thread-pool support for linux.

--- trunk/src/config.h
+++ trunk/src/config.h
@@ -21,7 +21,7 @@
 
 /************************* Compile time Limits/Options ***********************/
 
-#define VERSION "MiniWebsvr/0.0.7svn"
+#define VERSION "MiniWebsvr/0.0.7"
 #define COPYRIGHT "Copyright (C) 2007  Nickolas Antonie Grigoriadis"
 #define EMAIL "grigi_ at users.sourceforge.net"
 
