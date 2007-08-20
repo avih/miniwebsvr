e748b1b7610e3f3a5a3c364dab3fca37dc482bb6
grigi_ <grigi_@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-08-20 10:00:00 +0000

    Forced Win32 build to use 64-bit file functions

--- src/os_compat.h
+++ src/os_compat.h
@@ -19,7 +19,7 @@
 #ifndef OS_COMPAT_H
 #define OS_COMPAT_H
 
-// Define the 64-bit integer used for seeking
+// Define that I WANT 64-bit file oprations
 #define _FILE_OFFSET_BITS 64
 #define _LARGE_FILES
 
@@ -31,8 +31,11 @@
 
 	#define fseeko fseeko64
 	#define ftello ftello64
+	#define stat _stat64
+	#define fopen fopen64
 
 	#define socklen_t int
+	#define INT64 __int64
 
 	#define SEND_FLAG 0	// Windows does not have a similar flag?
 
@@ -60,22 +63,16 @@
 
 #endif // !__WINDOWS__
 
-// Define the 64-bit integer used for seeking
+// Define the 64-bit integer used for seeking if not defined yet
 #ifndef INT64
+#ifdef __int64
+#define INT64 __int64
+#else // __int64
 #ifdef __GNUC__
 #define INT64 long long
-#endif
-
-#ifdef _MSC_VER
-#if _MSC_VER >= 1100
-#define INT64   __int64
-#endif
-#endif
-
-#ifdef __BORLANDC__
-#define INT64 __int64
-#endif
-#endif
+#endif // __GNUC__
+#endif // __int64
+#endif // INT64
 
 #ifndef INT64
 #warning "No 64-bit type found, 2GB limit on files will apply"
