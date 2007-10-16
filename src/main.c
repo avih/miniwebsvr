ca4a799df967690410c850a8596b0687d6ede2a2
fyrewolfe <fyrewolfe@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-10-16 20:07:19 +0000

    Initial xml parsing.
    - The explode() function will return an array according to given delimiters on a char*.
    - The freeArray() function will free the memory for the array returned by explode()
    - The cleanup() function will free the memory from the xmlDoc struct. Incomplete.
    - parse() will fill the xmlDoc structure. Incomplete
    - subp() will return a sub char* from a given char* (e.g. "root" from "<root>"). Not tested yet.

--- src/main.c
+++ src/main.c
@@ -1,156 +1,187 @@
-/*  MiniWebSvr - A small webserver
-    Copyright (C) 2007  Nickolas Antonie Grigoriadis
-    E-Mail: grigi_ at users.sourceforge.net
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-#include "os_compat.h"
-
-#include <stdlib.h>
 #include <stdio.h>
-#include <signal.h>
+#include "xml.h"
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
-#ifdef __WIN32__
-#include <dir.h>
-#endif
+char buffer[255] = "";
+struct xmlDoc xml;
 
-#include "config.h"
-#include "listener.h"
-#include "logging.h"
+int aTask = 0;
 
-void cleanup()
+#define APP_DEBUG 1
+
+int main() 
 {
-#ifdef __WIN32__
-	WSACleanup();
-#endif
-	BIGMessage("--- %s terminated cleanly ---",VERSION);
-	StopLogging();
+    sprintf(xml.version,"");
+    sprintf(xml.encoding,"");
+    
+    //TODO: read lines from file
+    char line[] = "<?xml version=\"1.0\" encoding='UTF-8'?>\n<!--Comment-->\n<root>\n<main color=\"#FF9900\">Main Value</main>\n<woof meeaw /></root>\n";
+    #if APP_DEBUG    
+        printf("Size of line: %d\n",sizeof(line));
+        printf("String Length: %d\n\n",strlen(line));        
+    #endif
+    
+    parse(line,0);
+    //cleanup    
+    cleanup();
+    
+    int input = 0;
+    scanf("%d",&input);
+    
+    return 0;
 }
 
-// As documented at http://www.bindview.com/Services/Razor/Papers/2001/signals.cfm
-// There exists the possibility of a race condition if 2 or more different
-// signals are sent to the process nearly the same time when using the same
-// signal handler for different signals.
-void catch_int(int sig)
+char* subp(char* aSubject,int aStart, int aLength)
 {
-	BIGMessage("Caught signal SIGINT");
-	loop=0;
-	signal(sig,SIG_IGN);
+      char* retVal = 0;
+ 
+      if(aLength > (strlen(aSubject) - aStart)) aLength = strlen(aSubject);
+ 
+      retVal = malloc(aLength+1);
+      
+      int i=0;
+      for(i=0;i<aLength;i++)
+      {
+       retVal[i] = aSubject[i];
+      }
+ 
+      return retVal;     
 }
 
-void catch_term(int sig)
+int parse(char* aLine, char* xPath)
 {
-	BIGMessage("Caught signal SIGTERM");
-	loop=0;
-	signal(sig,SIG_IGN);
+    //TODO: parse accoring to xPath
+    int aLineSize = 0;
+    char** aline = explode(lang_delimiters,aLine,&aLineSize);
+    int i = 0;
+    for(i=0;i<aLineSize;i++)
+    {
+        switch(aTask)
+        {
+            case 0: default:
+            {
+                //Get XML Declaration
+                char * pch;
+                pch = strstr(aline[i],"xml");
+                 #if APP_DEBUG
+                     if(pch != 0) 
+                     {
+                      printf(">>> xml declaration found <<<\n");
+                      printf("Line: %s\n",aline[i]);
+                     }
+                #endif
+                //TODO: getVersion
+                //TODO: getEncoding
+                aTask++;
+            }break;
+            case 1:
+            {
+             #if APP_DEBUG
+                  printf("TODO: Parse Element\n");
+                  printf("Line: %s\n",aline[i]);
+             #endif
+            }break;
+        }
+    }
+      
+    if(aTask == 0) printf("Unable to locate xml declaration\n");
+    freeArray(aline,aLineSize);
 }
 
-#ifdef __WIN32__
-void catch_break(int sig)
+void cleanup(void)
 {
-	BIGMessage("Caught signal SIGBREAK");
-	loop=0;
-	signal(sig,SIG_IGN);
-}
-#else
-void catch_hup(int sig)
-{
-	BIGMessage("Caught signal SIGHUP");
-	loop=0;
-	signal(sig,SIG_IGN);
-}
-
-void catch_quit(int sig)
-{
-	BIGMessage("Caught signal SIGQUIT");
-	loop=0;
-	signal(sig,SIG_IGN);
-}
-#endif
-
-#ifdef USEWINMAIN
-#include <shellapi.h>
-
-char **argv = NULL;
-	
-/*******************************************************
-WIN32 command line parser function
-********************************************************/
-int ParseCommandline()
-{
-	int    argc, BuffSize, i;
-	WCHAR  *wcCommandLine;
-	LPWSTR *argw;
-	
-	// Get a WCHAR version of the parsed commande line
-	wcCommandLine = GetCommandLineW();	
-	argw = CommandLineToArgvW( wcCommandLine, &argc);
-
-	// Create the first dimension of the double array
-	argv = (char **)GlobalAlloc( LPTR, argc + 1 );
-	
-	// convert each line of wcCommandeLine to MultiByte and place them
-	// to the argv[] array
-	for( i=0; i < argc; i++)
-	{
-		BuffSize = WideCharToMultiByte( CP_ACP, WC_COMPOSITECHECK, argw[i], -1, NULL, 0, NULL, NULL );
-		argv[i] = (char *)GlobalAlloc( LPTR, BuffSize );		
-		WideCharToMultiByte( CP_ACP, WC_COMPOSITECHECK, argw[i], BuffSize * sizeof( WCHAR ) ,argv[i], BuffSize, NULL, NULL );
-	}
-
-	LocalFree(argw);
-
-	// return the number of argument
-	return argc;
+     if(xml.version != 0)
+     {
+        free(xml.version);
+        xml.version = 0;
+     }
+     
+     if(xml.encoding != 0)
+     {
+        free(xml.encoding);
+        xml.encoding = 0;
+     }
+	 
+	 //TODO: Elemets cleanup
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+void freeArray(char** aArray,int aSize)
 {
-	int argc = ParseCommandline( );
-#else
-int main(int argc, char **argv)
+    int i = 0;
+    for(i =0 ;i < aSize; i++)
+    {
+     char* tmpPtr = (char *)aArray[i];
+     free(tmpPtr);
+    }
+    free(aArray);
+}
+
+char** explode(char* delimeters,char* string, int* aSize)
 {
-#endif
+        char** stringArray = 0;
+        int aCount = 1;
+        
+        char* buffer = strtok(string,delimeters);
+        
+        while(buffer != 0)
+        {
+           if(aCount > 1) 
+           {
+            char** tmpArr = calloc(aCount,sizeof(char*));
+            //assert(tmpArr);
+            if(tmpArr == 0)
+            {
+              #if APP_DEBUG
+                  printf("Memory Error in explode('%s','%s','%d') on tmpArr\n",delimeters,string,aSize);
+              #endif
+              return(0);
+            }
+            memcpy(tmpArr,stringArray,(aCount-1)*sizeof(char*));
+            free(stringArray);
+            stringArray = 0;
+            stringArray = calloc(aCount,sizeof(char*));
+            //assert(stringArray);
+            if(stringArray == 0)
+            {
+              #if APP_DEBUG
+                  printf("Memory Error in explode('%s','%s','%d') on stringArray\n",delimeters,string,aSize);
+              #endif
+              return(0);
+            }
+            memcpy(stringArray,tmpArr,(aCount-1)*sizeof(char*));
+            free(tmpArr);
+            tmpArr = 0;
+            char* tmpbuff = malloc(strlen(buffer)+1); //+1 for \0 terminator
+            //assert(tmpbuff);
+            if(tmpbuff == 0)
+            {
+              #if APP_DEBUG
+                  printf("Memory Error in explode('%s','%s','%d') on tmpbuff\n",delimeters,string,aSize);
+              #endif
+              return(0);
+            }
+            stringArray[aCount-1] = memcpy ( tmpbuff, buffer, strlen(buffer)+1 );
+           }
+           else
+           {
+              stringArray = calloc(aCount,sizeof(char*)); //memory block = (aCount * sizeof(char *))
+              char* tmpbuff = malloc(strlen(buffer)+1); //+1 for \0 terminator
+              //assert(tmpbuff);
+              if(tmpbuff == 0)
+              {
+                #if APP_DEBUG
+                printf("Memory Error in explode('%s','%s','%d') on tmpbuff\n",delimeters,string,aSize);
+                #endif
+              }
+              stringArray[aCount-1] = memcpy ( tmpbuff, buffer, strlen(buffer)+1 );
+           }
+           
+         aCount++;
+         buffer = strtok(0,delimeters);
+        }
 
-	getconfig(argc, argv);
-
-	if (atexit(&cleanup))
-	{
-		#ifdef USEWINMAIN
-		MessageBox(NULL, "Cannot set exit function", "Critical", MB_ICONERROR);
-		#else
-		fprintf(stderr, "Critical: Cannot set exit function\n");
-		#endif
-		return EXIT_FAILURE;
-	}
-	signal (SIGINT, &catch_int);
-	signal (SIGTERM, &catch_term);
-#ifdef __WIN32__
-	signal (SIGBREAK, &catch_break);
-#else
-	signal (SIGHUP, &catch_hup);
-	signal (SIGQUIT, &catch_quit);
-#endif
-
-	StartLogging(LOGFILE);
-	BIGMessage("--- %s starting ---",VERSION);
-	BIGMessage("--- This software is distributed under the GNU Lesser General Public License");
-	BIGMessage("--- %s",COPYRIGHT);
-	BIGMessage("--- E-Mail: %s",EMAIL);
-
-// chdir to the document root
-	chdir(ROOT);
-	return listener(INTERFACE,PORT);
+        *aSize = aCount-1;
+        return stringArray;
 }
