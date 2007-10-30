1ab873edfa2d8e96879eeae4b81e7c8c4787a23b
fyrewolfe <fyrewolfe@9c9cb875-1127-0410-b8f7-bc7b75a7ccb3>
2007-10-30 14:32:06 +0000

    Cleaned up the code
    - Added License.txt
    - Added license to .c & .h files
    - moved xml specific code to xml.c
    - renamed cleanup() to xmlCleanup()
    - xmlCleanup() will received the pointer of the xml document to clean
    - renamed parse() to parseXml()

--- branches/xml/src/main.c
+++ branches/xml/src/main.c
@@ -1,15 +1,28 @@
-#include <stdio.h>
-#include "xml.h"
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+/*  MiniWebsvr XML - A small xml parser
+    Copyright (C) 2007  Nickolas Antonie Grigoriadis and Johan Strydom
+    E-Mail: grigi_ at users.sourceforge.net and fyrewolfe at users.sourceforge.net
 
-char buffer[255] = "";
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include "xml.h"
+
 struct xmlDoc xml;
 
-int aTask = 0;
-
-#define APP_DEBUG 1
+#ifndef APP_DEBUG
+#define APP_DEBUG
+#endif
 
 int main() 
 {
@@ -18,170 +31,17 @@ int main()
     
     //TODO: read lines from file
     char line[] = "<?xml version=\"1.0\" encoding='UTF-8'?>\n<!--Comment-->\n<root>\n<main color=\"#FF9900\">Main Value</main>\n<woof meeaw /></root>\n";
-    #if APP_DEBUG    
+    #ifdef APP_DEBUG    
         printf("Size of line: %d\n",sizeof(line));
         printf("String Length: %d\n\n",strlen(line));        
     #endif
     
-    parse(line,0);
+    parseXml(line,0);
     //cleanup    
-    cleanup();
+    xmlCleanup(&xml);
     
     int input = 0;
     scanf("%d",&input);
     
     return 0;
-}
-
-char* subp(char* aSubject,int aStart, int aLength)
-{
-      char* retVal = 0;
- 
-      if(aLength > (strlen(aSubject) - aStart)) aLength = strlen(aSubject);
- 
-      retVal = malloc(aLength+1);
-      
-      int i=0;
-      for(i=0;i<aLength;i++)
-      {
-       retVal[i] = aSubject[i];
-      }
- 
-      return retVal;     
-}
-
-int parse(char* aLine, char* xPath)
-{
-    //TODO: parse accoring to xPath
-    int aLineSize = 0;
-    char** aline = explode(lang_delimiters,aLine,&aLineSize);
-    int i = 0;
-    for(i=0;i<aLineSize;i++)
-    {
-        switch(aTask)
-        {
-            case 0: default:
-            {
-                //Get XML Declaration
-                char * pch;
-                pch = strstr(aline[i],"xml");
-                 #if APP_DEBUG
-                     if(pch != 0) 
-                     {
-                      printf(">>> xml declaration found <<<\n");
-                      printf("Line: %s\n",aline[i]);
-                     }
-                #endif
-                //TODO: getVersion
-                //TODO: getEncoding
-                aTask++;
-            }break;
-            case 1:
-            {
-             #if APP_DEBUG
-                  printf("TODO: Parse Element\n");
-                  printf("Line: %s\n",aline[i]);
-             #endif
-            }break;
-        }
-    }
-      
-    if(aTask == 0) printf("Unable to locate xml declaration\n");
-    freeArray(aline,aLineSize);
-}
-
-void cleanup(void)
-{
-     if(xml.version != 0)
-     {
-        free(xml.version);
-        xml.version = 0;
-     }
-     
-     if(xml.encoding != 0)
-     {
-        free(xml.encoding);
-        xml.encoding = 0;
-     }
-	 
-	 //TODO: Elemets cleanup
-}
-
-void freeArray(char** aArray,int aSize)
-{
-    int i = 0;
-    for(i =0 ;i < aSize; i++)
-    {
-     char* tmpPtr = (char *)aArray[i];
-     free(tmpPtr);
-    }
-    free(aArray);
-}
-
-char** explode(char* delimeters,char* string, int* aSize)
-{
-        char** stringArray = 0;
-        int aCount = 1;
-        
-        char* buffer = strtok(string,delimeters);
-        
-        while(buffer != 0)
-        {
-           if(aCount > 1) 
-           {
-            char** tmpArr = calloc(aCount,sizeof(char*));
-            //assert(tmpArr);
-            if(tmpArr == 0)
-            {
-              #if APP_DEBUG
-                  printf("Memory Error in explode('%s','%s','%d') on tmpArr\n",delimeters,string,aSize);
-              #endif
-              return(0);
-            }
-            memcpy(tmpArr,stringArray,(aCount-1)*sizeof(char*));
-            free(stringArray);
-            stringArray = 0;
-            stringArray = calloc(aCount,sizeof(char*));
-            //assert(stringArray);
-            if(stringArray == 0)
-            {
-              #if APP_DEBUG
-                  printf("Memory Error in explode('%s','%s','%d') on stringArray\n",delimeters,string,aSize);
-              #endif
-              return(0);
-            }
-            memcpy(stringArray,tmpArr,(aCount-1)*sizeof(char*));
-            free(tmpArr);
-            tmpArr = 0;
-            char* tmpbuff = malloc(strlen(buffer)+1); //+1 for \0 terminator
-            //assert(tmpbuff);
-            if(tmpbuff == 0)
-            {
-              #if APP_DEBUG
-                  printf("Memory Error in explode('%s','%s','%d') on tmpbuff\n",delimeters,string,aSize);
-              #endif
-              return(0);
-            }
-            stringArray[aCount-1] = memcpy ( tmpbuff, buffer, strlen(buffer)+1 );
-           }
-           else
-           {
-              stringArray = calloc(aCount,sizeof(char*)); //memory block = (aCount * sizeof(char *))
-              char* tmpbuff = malloc(strlen(buffer)+1); //+1 for \0 terminator
-              //assert(tmpbuff);
-              if(tmpbuff == 0)
-              {
-                #if APP_DEBUG
-                printf("Memory Error in explode('%s','%s','%d') on tmpbuff\n",delimeters,string,aSize);
-                #endif
-              }
-              stringArray[aCount-1] = memcpy ( tmpbuff, buffer, strlen(buffer)+1 );
-           }
-           
-         aCount++;
-         buffer = strtok(0,delimeters);
-        }
-
-        *aSize = aCount-1;
-        return stringArray;
 }
--- branches/xml/src/xml.h
+++ branches/xml/src/xml.h
@@ -1,35 +1,55 @@
+/*  MiniWebsvr XML - A small xml parser
+    Copyright (C) 2007  Nickolas Antonie Grigoriadis and Johan Strydom
+    E-Mail: grigi_ at users.sourceforge.net and fyrewolfe at users.sourceforge.net
 
-struct xml_attribute
-{
-   char* name;
-   char* value;
-};
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-struct xml_element
-{
- char* name;
- char* text;
- int size_attributes;
- int size_children;
- struct xml_attribute** attributes;
- struct xml_element** children;
- struct xml_element* parent;
-};
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-struct xmlDoc
-{
-     char* version;
-     char* encoding;
-     struct xml_element* root;  
-};
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
-int parse(char *, char *);
-char** explode(char *,char *,int*);
-char* subp(char*,int, int);
+#ifndef XML_H
+#define XML_H
 
-char lang_delimiters[] = "><\n?";
-char ws_delimeters[] = "<\n\"' ";
-
-void freeArray(char**,int);
-
-void cleanup(void);
+    struct xml_attribute
+    {
+       char* name;
+       char* value;
+    };
+    
+    struct xml_element
+    {
+     char* name;
+     char* text;
+     int size_attributes;
+     int size_children;
+     struct xml_attribute** attributes;
+     struct xml_element** children;
+     struct xml_element* parent;
+    };
+    
+    struct xmlDoc
+    {
+         char* version;
+         char* encoding;
+         struct xml_element* root;  
+    };
+    
+    int parseXml(char *, char *);
+    char** explode(char *,char *,int*);
+    char* subp(char*,int, int);
+    
+    void freeArray(char**,int);
+    
+    void xmlCleanup(struct xmlDoc*);
+    
+#endif
